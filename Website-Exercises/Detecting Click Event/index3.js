fca7e5976373659fc645f4bead3ada1c61943b7d
MusaAkyuz <musakyuz0@gmail.com>
2023-01-17 10:59:00 +0300

    reoptimizing javascript

--- Website-Exercises/Detecting Click Event/index3.js
+++ Website-Exercises/Detecting Click Event/index3.js
@@ -1,24 +1,62 @@
 let col1 = document.getElementById("column1");
-let msg = document.getElementById("msg")
+let msg = document.getElementById("msg");
 
 const buttons = document.getElementsByTagName("*");
 
 const buttonPressed = e => {
     clickedTarget = e.target;  // Get ID of Clicked Element
-    msg.textContent = ""
+    msg.textContent = "";
+
     while(clickedTarget.tagName !== "html")
     {
+        index = 0
+        var tagArray = []
         var parent = clickedTarget.parentNode;
-        var index = Array.prototype.indexOf.call(parent.children, clickedTarget)
-        if(index > 0)
+        var childNodes = parent.childNodes;
+        var count = childNodes.length;
+        var child_index = 0;
+        var i = 0;
+
+        for (i = 0; i < count; ++i) {
+            if(clickedTarget.tagName === childNodes[i].tagName) {
+                tagArray.push(childNodes[i]);
+                child_index++
+            }
+        }
+
+        for(i = 0; i < child_index; i++)
         {
-            msg.textContent += clickedTarget.tagName + " : ntn-child(" + (index+1) + ") "
-            clickedTarget = clickedTarget.parentElement
+            if(clickedTarget === tagArray[i])
+            {
+                index = i;
+                break;
+            }
+        }
+        //index = child_index;
+        /*
+        node = clickedTarget;
+        index = 0;
+        //var tagN = clickedTarget.tagName
+        while(node != clickedTarget.parentElement)
+        {
+            if(node.tagName == clickedTarget.tagName)
+            {
+                index++;
+            }
+            node = node.previousSibling;
+        }
+        */
+        //if(index > 0)
+        //{
+            msg.textContent += clickedTarget.tagName + " : ntn-child(" + (index+1) + ") ";
+            //msg.textContent = child_index
+            clickedTarget = clickedTarget.parentElement;
             if(clickedTarget.tagName === "html")
             {
                 break;
             }
-        }
+        //}
+        /*
         else
         {
             msg.textContent += clickedTarget.tagName + " "
@@ -28,6 +66,7 @@ const buttonPressed = e => {
                 break;
             }
         }
+        */
     }
 }
 
