72bc082dc409f2b5d1e4cc725e68ef8295fa36fc
Musa Akyüz <62879859+MusaAkyuz@users.noreply.github.com>
2023-08-09 17:34:17 +0300

    Update animation.js

--- Website-Exercises/Peronal-Website/animation.js
+++ Website-Exercises/Peronal-Website/animation.js
@@ -1,50 +1,38 @@
-//typewriter animation
-// set up text to print, each item in array is new line
+var aText = [
+    "Musa Akyüz", 
+    "Jr. Cyber Analyst"
+];
 
+var iSpeed = 140;
+var iIndex = 0;
+var iArrLength = aText[0].length;
+var iScrollAt = 20;
+var iTextPos = 0;
+var sContents = '';
+var iRow;
 
-var aText = new Array(
-"Musa Akyüz", 
-"Jr. Cyber Analyst"
-);
+function typewriter() {
+    sContents = ' ';
+    iRow = Math.max(0, iIndex - iScrollAt);
+    var destination = document.getElementById("typedtext");
 
+    while (iRow < iIndex) {
+        sContents += aText[iRow++] + '<br>';
+    }
 
-var iSpeed = 140; // time delay of print out
-var iIndex = 0; // start printing array at this posision
-var iArrLength = aText[0].length; // the length of the text array
-var iScrollAt = 20; // start scrolling up at this many lines
- 
- 
- 
-var iTextPos = 0; // initialise text position
-var sContents = ''; // initialise contents variable
-var iRow; // initialise current row
+    destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
 
- 
-function typewriter(musa)
-{
- sContents =  ' ';
- iRow = Math.max(0, iIndex-iScrollAt);
- var destination = document.getElementById("typedtext");
- //var destination2 = document.getElementById("typedtext2");
- //var destination3 = document.getElementById("typedtext3");
- 
- while ( iRow < iIndex ) {
-  sContents += musa[iRow++] + '<br>';
- }
- destination.innerHTML = sContents + musa[iIndex].substring(0, iTextPos) + "_";
- if ( iTextPos++ == iArrLength ) {
-  iTextPos = 0;
-  iIndex++;
-  if ( iIndex != musa.length ) {
-   iArrLength = musa[iIndex].length;
-   setTimeout("typewriter()", 700);
-  }
- } else {
-  setTimeout("typewriter()", iSpeed);
- } 
+    if (iTextPos++ == iArrLength) {
+        iTextPos = 0;
+        iIndex++;
+
+        if (iIndex != aText.length) {
+            iArrLength = aText[iIndex].length;
+            setTimeout(typewriter, 700);
+        }
+    } else {
+        setTimeout(typewriter, iSpeed);
+    }
 }
 
-
-typewriter(aText);
-
-
+typewriter();
